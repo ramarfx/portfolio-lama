562235f47fc496071b4fcf31b15066afb27fdc09
ramarfx <ramagamiing33@gmail.com>
2023-07-03 00:08:55 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+//hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#navMenu')
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('hamburger-active');
+	navMenu.classList.toggle('hidden');
+
+});
+
+//navabr fixed
+window.onscroll = function () {
+	const header = document.querySelector('header');
+	const fixedNav = header.offsetTop;
+
+   if (window.scrollY > fixedNav) {
+      header.classList.add('navbar-fixed')
+   }else{
+      header.classList.remove('navbar-fixed')
+   }
+};
